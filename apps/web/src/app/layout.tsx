ed33ca8ca4f99fbd0dc568259b9ac121bec70d44
Paul Lam <lamkashingpaul@gmail.com>
2025-11-09 01:55:57 +0800

    feat: add Tailwind CSS configuration, global styles, and new button component; remove unused components

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@workspace/ui/globals.css";
 import { headers } from "next/headers";
 import { cookieToInitialState } from "wagmi";
 import { Providers } from "@/features/providers/providers";
--- apps/web/src/features/providers/providers.tsx
+++ apps/web/src/features/providers/providers.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type ReactNode, useState } from "react";
 import { type State, WagmiProvider } from "wagmi";
